b6d1eee4298639fa8814cd10407000f986cf34e9
samiullah58 <samiullahrashid4@gmail.com>
2023-08-07 17:51:27 +0500

    checking env file access

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const express = require("express");
 
 const app = express();
 dotenv.config().parsed;
+const PORT = process.env.PORT;
+const dbUri = process.env.URI;
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(bodyParser.urlencoded({ extended: true }));
-const PORT = process.env.PORT;
-// const dbUri = process.env.URI;
 
 app.get("/", (req, res) => {
   res.setHeader("Content-Type", "application/json");
@@ -21,11 +21,11 @@ require("./startup/route")(app);
 
 async function connectToDatabase() {
   try {
-    await mongoose.connect(process.env.URI, {
+    await mongoose.connect(dbUri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log("dbUri:", process.env.URI);
+    console.log("dbUri:", dbUri);
     console.log("Connected to MongoDB");
   } catch (err) {
     console.error("Error connecting to MongoDB:", err);
@@ -33,6 +33,6 @@ async function connectToDatabase() {
 }
 
 connectToDatabase();
-app.listen(process.env.PORT, () => {
-  console.log(`Server started on port: ${process.env.PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server started on port: ${PORT}`);
 });
