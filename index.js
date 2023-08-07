787892a9ad7dc1c337f6a6ebbc0a0df0357e7a03
samiullah58 <samiullahrashid4@gmail.com>
2023-08-07 18:06:28 +0500

    checking env file access

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ const express = require("express");
 
 const app = express();
 dotenv.config().parsed;
-// const port = process.env.PORT;
-// const dbUri = process.env.URI;
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -25,7 +23,6 @@ async function connectToDatabase() {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    // console.log("dbUri:", dbUri);
     console.log("Connected to MongoDB");
   } catch (err) {
     console.error("Error connecting to MongoDB:", err);
