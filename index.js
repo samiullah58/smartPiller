ebf4a5666da8184d3d74c2be1d70b233b9c64025
samiullah58 <samiullahrashid4@gmail.com>
2023-08-07 18:30:34 +0500

    checking env file access

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const dotenv = require("dotenv");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+const { MongoClient } = require("mongodb");
 const express = require("express");
 
-dotenv.config({ path: __dirname + "/.env" });
+dotenv.config();
 
 const app = express();
 app.use(cors());
@@ -20,11 +20,11 @@ require("./startup/route")(app);
 
 async function connectToDatabase() {
   try {
-    console.log("MongoDB URI:", process.env.URI);
-    await mongoose.connect(process.env.URI, {
+    const client = new MongoClient(process.env.URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
+    await client.connect();
     console.log("Connected to MongoDB");
   } catch (err) {
     console.error("Error connecting to MongoDB:", err);
@@ -35,3 +35,41 @@ connectToDatabase();
 app.listen(process.env.PORT, () => {
   console.log(`Server started on port: ${process.env.PORT}`);
 });
+
+// const dotenv = require("dotenv");
+// const cors = require("cors");
+// const bodyParser = require("body-parser");
+// const mongoose = require("mongoose");
+// const express = require("express");
+
+// dotenv.config({ path: __dirname + "/.env" });
+
+// const app = express();
+// app.use(cors());
+// app.use(express.json({ limit: "10mb" }));
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.get("/", (req, res) => {
+//   res.setHeader("Content-Type", "application/json");
+//   res.send("this is smart piller");
+// });
+
+// require("./startup/route")(app);
+
+// async function connectToDatabase() {
+//   try {
+//     console.log("MongoDB URI:", process.env.URI);
+//     await mongoose.connect(process.env.URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log("Connected to MongoDB");
+//   } catch (err) {
+//     console.error("Error connecting to MongoDB:", err);
+//   }
+// }
+
+// connectToDatabase();
+// app.listen(process.env.PORT, () => {
+//   console.log(`Server started on port: ${process.env.PORT}`);
+// });
