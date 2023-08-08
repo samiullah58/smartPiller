2446dc799052468f9c9d830ef9d54f8d910c6ac1
samiullah58 <samiullahrashid4@gmail.com>
2023-08-08 11:49:30 +0500

    testing

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const express = require("express");
 
 // dotenv.config({ path: __dirname + "/.env" });
 
+// console.log(process.env.NODE_ENV);
+
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
@@ -20,7 +22,7 @@ require("./startup/route")(app);
 
 async function connectToDatabase() {
   try {
-    await mongoose.connect("mongodb+srv://samiullahbcs5th5450:pWyhHqqAzwANA8Fm@cluster0.awsc1pv.mongodb.net/smartPiller?retryWrites=true&w=majority", {
+    await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
@@ -31,6 +33,6 @@ async function connectToDatabase() {
 }
 
 connectToDatabase();
-app.listen(3000, () => {
-  console.log(`Server started on port: 3000`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server started on port: ${process.env.PORT}`);
 });
