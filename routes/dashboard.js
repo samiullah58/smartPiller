8929553c3bfe3458d652f49d7d47dcdf1caec20c
samiullah58 <samiullahrashid4@gmail.com>
2023-08-08 15:10:03 +0500

    dashboard route has been added

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,13 +1,25 @@
 const { Dashboard, validate } = require("../model/dashboard");
-const Property = require("../model/property");
+const { Property } = require("../model/property");
+const { LeaseProperty } = require("../model/leaseProperty");
+const { Maintains } = require("../model/maintains");
+const { Tenants } = require("../model/tenants");
 const express = require("express");
 const router = express.Router();
 router.use(express.json());
 
 router.get("/", async (req, res) => {
-  //   console.log();
-  const dashboard = await Property.find();
-  res.json({ data: dashboard.length });
+  const property = await Property.find();
+  const leaseProperty = await LeaseProperty.find();
+  const maintains = await Maintains.find();
+  const tenants = await Tenants.find();
+  res.json({
+    data: {
+      totalProperty: property.length,
+      totalLeaseProperty: leaseProperty.length,
+      totalMaintains: maintains.length,
+      totalTenants: tenants.length,
+    },
+  });
 });
 
 module.exports = router;
