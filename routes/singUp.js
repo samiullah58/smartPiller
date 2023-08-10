ea7d03db3a43343f242d1f05a1e2b9ccd95fc902
samiullah58 <samiullahrashid4@gmail.com>
2023-08-10 13:12:09 +0500

    remove authentication from put  request

--- routes/singUp.js
+++ routes/singUp.js
@@ -50,7 +50,7 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.put("/:id", auth, async (req, res) => {
+router.put("/:id", async (req, res) => {
   try {
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
